252f3858558ed8eadb3c2529531bb5a215248d99
Joe Du <joeys.du@gmail.com>
2026-03-24 00:58:23 +1100

    first commit

---
+++ CrashCourse/Program.cs
@@ -0,0 +1,17 @@
+﻿using CrashCourse.Models;
+
+Computer computer = new Computer()
+{
+    Motherboard = "Z690",
+    HasWifi = true,
+    ReleaseDate = DateTime.Now,
+    Price = 943.87m,
+    VideoCard = "RTX2060"
+};
+
+Console.WriteLine(computer.Motherboard);
+Console.WriteLine(computer.HasWifi);
+Console.WriteLine(computer.ReleaseDate);
+
+
+
